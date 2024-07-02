dbca3c1e42cffaf28a954f569f27032df8a00ee7
Nguyen Hong Tuan PHAT <142778806+PhatB2111894@users.noreply.github.com>
2024-07-02 21:28:27 +0700

    Update

---
+++ partials/footer.php
@@ -0,0 +1,123 @@
+<!-- Footer -->
+<link rel="stylesheet" href="./css/footer.css">
+<footer>
+    <div class="text-center text-lg-start text-dark">
+        <!-- Section: Social media -->
+        <section class="d-flex justify-content-center justify-content-lg-between p-3 border-bottom">
+            <!-- Left -->
+            <div class="me-5 d-none d-lg-block">
+                <span class="fst-italic fs-5">Hãy kết nối với Thời Số</span>
+            </div>
+            <!-- Left -->
+            <!-- Right -->
+            <div>
+                <a href="" class="me-4 text-reset">
+                    <i class="text-color text-decoration-none fab fa-facebook-f"></i>
+                </a>
+                <a href="" class="me-4 text-reset">
+                    <i class="text-color text-decoration-none fab fa-twitter"></i>
+                </a>
+                <a href="" class="me-4 text-reset">
+                    <i class="text-color text-decoration-none fab fa-google"></i>
+                </a>
+                <a href="" class="me-4 text-reset">
+                    <i class="text-color text-decoration-none fab fa-instagram"></i>
+                </a>
+                <a href="" class="me-4 text-reset">
+                    <i class="text-color text-decoration-none fab fa-linkedin"></i>
+                </a>
+                <a href="" class="me-4 text-reset">
+                    <i class="text-color text-decoration-none fab fa-github"></i>
+                </a>
+            </div>
+            <!-- Right -->
+        </section>
+        <!-- Section: Social media -->
+
+        <!-- Section: Links  -->
+        <section class="">
+            <div class="container text-center text-md-start mt-5">
+                <!-- Grid row -->
+                <div class="row mt-3">
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4 mt--32">
+                        <!-- Content -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            <a href="#"><img src="./picture/logo.png" alt="" class="w-50"></a>
+                        </h6>
+                        <p>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Thông tin về Shop
+                        </h6>
+                        <p class="footer-item">
+                            <a href="./index.php?id=home#" class="text-reset text-decoration-none">Trang Chủ </a>
+                        </p>
+                        <p class="footer-item">
+                            <a href="./hoa.php?id=hoa#" class="text-reset text-decoration-none">Thiết bị điện tử </a>
+                        </p>
+                        <p class="footer-item">
+                            <a href="./hoacuoi.php?id=hoacuoi#" class="text-reset text-decoration-none">Linh kiện điện tử</a>
+                        </p>
+                        <p class="footer-item">
+                            <a href="./cart.php" class="text-reset text-decoration-none">Giỏ Hàng</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Hệ Thống Chi Nhánh
+                        </h6>
+                        <p class="footer-item">
+                            <a href="#!" class="text-reset text-decoration-none ">51A2 - Đ 3/2 - P. Xuân Khánh - Q. Ninh Kiều - TP Cần Thơ</a>
+                        </p>
+                        <p class="footer-item">
+                            <a href="#!" class="text-reset text-decoration-none ">30/48K - Đ Nguyễn Văn Linh - P. Hưng Lợi - Q. Ninh Kiều - TP Cần Thơ</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">Địa chỉ liên hệ</h6>
+                        <p class="footer-item"><i class="text-color fas fa-home me-3"></i>30/48K - Đ Nguyễn Văn Linh - P. Hưng Lợi - Q. Ninh Kiều - TP Cần Thơ</p>
+                        <p class="footer-item">
+                            <i class="text-color fas fa-envelope me-3"></i>
+                            ctythoiso@gmail.com
+                        </p>
+                        <p class="footer-item"><i class="text-color fas fa-phone me-3"></i> Hotline: 0968 60 67 67</p>
+
+                    </div>
+                    <!-- Grid column -->
+                </div>
+                <!-- Grid row -->
+            </div>
+        </section>
+        <!-- Section: Links  -->
+
+        <!-- Copyright -->
+        <div class="text-center p-2" style="background-color: rgba(0, 0, 0, 0.05);">
+            <span class="fw-bold">&copy;</span> 2024 Copyright:
+            <a class="text-reset fw-bold" href="https://thoiso.vn/"> Thời Số</a>
+        </div>
+        <div id="backtop">
+            <i class="fa-solid fa-arrow-up"></i>
+        </div>
+        <!-- Copyright -->
+    </div>
+
+</footer>
+<!-- Footer -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="js/backtop.js"></script>
+
